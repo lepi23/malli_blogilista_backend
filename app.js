82f149e42e3d1e51ae795579b3c4cbdfeba50a88
Leevi Saarinen <leevi.saarinen@tuni.fi>
2020-07-18 17:12:19 +0300

    backend blogilistaa varten

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const app = express()
 
 mongoose.set('useCreateIndex', true)
 
+logger.info('connecting to', config.MONGODB_URI)
+
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info('connected to MongoDB')
@@ -29,6 +31,11 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.errorHandler)
 
